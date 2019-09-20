926fd9e200eed0f22265b233a2bb402e3ef3ff9d
Elian <cris.ufam2012@gmail.com>
2019-09-20 01:32:24 -0400

    Criacao do componente usuarios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Navbar from './componentes/layout/Navbar';
+import UserItem from './componentes/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render(){
-    return (
+     return (
      <div className='App'>
-       <Navbar/>
+        <Navbar />
+        <UserItem/>
      </div>
     );
   }
 }
 
 export default App;
+
--- src/componentes/layout/Navbar.js
+++ src/componentes/layout/Navbar.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+    
+
+    static defaultProps = {
+        title: 'Localizador Github',
+        icon: 'fab fa-github'
+    }
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
+    }
+    
     render() {
         return (
             <nav className="navbar bg-primary">
             <h1>
-                <i class="fab fa-github"/> Navbar
+                <i className={this.props.icon}/> {this.props.title}
             </h1>
             </nav>
         )
     }
 }
 
-export default Navbar
+export default Navbar;
+
